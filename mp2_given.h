874362c087daa829b4fc1be3349e2224e77c0d87
LegendCHN <ningkaiwu@gmail.com>
2019-03-25 13:05:21 -0500

    initial test

--- mp2_given.h
+++ mp2_given.h
@@ -20,4 +20,8 @@ struct task_struct* find_task_by_pid(unsigned int nr)
     return task;
 }
 
+static ssize_t mp2_write (struct file *file, const char __user *buffer, size_t count, loff_t *data);
+static ssize_t mp2_read (struct file *file, char __user *buffer, size_t count, loff_t *data);
+int __init mp2_init(void);
+void __exit mp2_exit(void);
 #endif
