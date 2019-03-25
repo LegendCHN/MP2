27ad5dd421dcccd186c0aa276b562e306015c46f
LegendCHN <ningkaiwu@gmail.com>
2019-03-25 14:51:08 -0500

    adding reg handler

--- mp2.c
+++ mp2.c
@@ -8,6 +8,7 @@
 #include <linux/uaccess.h>
 #include <linux/mutex.h> 
 #include <linux/slab.h>
+#include <linux/kthread.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Group_ID");
@@ -17,7 +18,9 @@ MODULE_DESCRIPTION("CS-423 MP2");
 #define DIRECTORY "mp2"
 #define DEBUG 1
 #define MASK 0666
-
+#define READY 0
+#define RUNNING 1
+#define SLEEPING 2
 // file operations for file status
 static const struct file_operations mp2_file = {
    .owner = THIS_MODULE,
@@ -32,6 +35,8 @@ static struct linkedlist reglist;
 static struct linkedlist *tmp;
 static struct list_head *pos, *q;
 static struct mutex lock;
+static struct kmem_cache *cache;
+static struct task_struct *dispatching_t;
 
 // helper function to free linkedlist space
 void free_linkedlist(void){
@@ -39,14 +44,15 @@ void free_linkedlist(void){
        tmp= list_entry(pos, struct linkedlist, list);
        list_del(pos);
        kfree(tmp);
+       kmem_cache_free(cache, tmp);
    }
 }
 
-// read function to show pid and cpu time
+// read function to show pid, period and processing time
 static ssize_t mp2_read (struct file *file, char __user *buffer, size_t count, loff_t *data){
    int copied;
    int length;
-   char * buf;
+   char *buf;
    buf = (char *) kmalloc(count, GFP_KERNEL);
    copied = 0;
    // destroy to prevent furthur read
@@ -68,19 +74,56 @@ static ssize_t mp2_read (struct file *file, char __user *buffer, size_t count, l
 
 // write function to add pid list entry to linkedlist
 static ssize_t mp2_write (struct file *file, const char __user *buffer, size_t count, loff_t *data){
-   // char buf[100];
-   // tmp = (struct linkedlist*)kmalloc(sizeof(struct linkedlist), GFP_KERNEL);
-   // copy_from_user(buf, buffer, count);
-   // // read in cpu number and initialize time to 0
-   // sscanf(buf, "%d", &tmp->pid);
-   // tmp->time = 0;
-   // // critical section to write
-   // mutex_lock(&lock);
-   // list_add(&(tmp->list), &(reglist.list));
-   // mutex_unlock(&lock);
+   char *buf;
+   buf = (char *)kmalloc(count, GFP_KERNEL);
+   copy_from_user(buf, buffer, count);
+   switch(buf[0]){
+      case 'R':
+         registration_handler(buf);
+         break;
+      case 'Y':
+         yield_handler(buf);
+         break;
+      case 'D':
+         de_registration_handler(buf);
+         break;
+   }
+   kfree(buf);
    return count;
 }
 
+void wakeup_f(unsigned int pid){
+   mutex_lock(&lock);
+   find_task_by_pid(pid)->state = READY;
+   mutex_unlock(&lock);
+   wake_up_process(dispatching_t);
+}
+
+// register handler
+void registration_handler(char *buf){
+   struct linkedlist *cur_task;
+   cur_task = (struct linkedlist *)kmem_cache_alloc(cache, GFP_KERNEL);
+   sscanf(buf, "%d %lu %lu", &cur_task->pid, &cur_task->period, &cur_task->computation);
+
+   cur_task->linux_task = find_task_by_pid(cur_task->pid);
+   cur_task->task_state = SLEEPING;
+   setup_timer(&(cur_task->wakeup_timer), wakeup_f, cur_task->pid);
+
+   mutex_lock(&lock);
+   list_add(&(tmp->list), &(reglist.list));
+   mutex_unlock(&lock);
+}
+// yield handler
+void yield_handler(char *buf){
+   
+}
+// de-register handler
+void de_registration_handler(char *buf){
+   
+}
+int dispatching_t_fn(void *data){
+   return 0;
+}
 // mp2_init - Called when module is loaded
 int __init mp2_init(void)
 {
@@ -95,6 +138,10 @@ int __init mp2_init(void)
    INIT_LIST_HEAD(&reglist.list);
    mutex_init(&lock);
 
+   // initialize cache
+   cache = kmem_cache_create("cache", sizeof (struct linkedlist), 0, SLAB_HWCACHE_ALIGN, NULL, NULL);
+   dispatching_t = kthread_create(dispatching_t_fn, NULL, "dispatching_t");
+
    printk(KERN_ALERT "MP2 MODULE LOADED\n");
    return 0;   
 }
@@ -106,6 +153,8 @@ void __exit mp2_exit(void)
    printk(KERN_ALERT "MP2 MODULE UNLOADING\n");
    #endif
 
+   kthread_stop(dispatching_t);
+   kmem_cache_destroy(cache);
    // destroy linked list
    free_linkedlist();
    // destroy dir and file
--- mp2_given.h
+++ mp2_given.h
@@ -4,22 +4,30 @@
 #include <linux/pid.h>
 #include <linux/sched.h>
 struct linkedlist{
-  struct list_head list;
-  int pid;
-  unsigned long time;
-  unsigned long period;
-  unsigned long computation;
+	struct list_head list;
+	struct task_struct* linux_task;
+	struct timer_list wakeup_timer;
+
+	unsigned int task_state;
+	unsigned int pid;
+	unsigned long time;
+	unsigned long period;
+	unsigned long computation;
 };
 
 struct task_struct* find_task_by_pid(unsigned int nr)
 {
-    struct task_struct* task;
-    rcu_read_lock();
-    task=pid_task(find_vpid(nr), PIDTYPE_PID);
-    rcu_read_unlock();
-    return task;
+	struct task_struct* task;
+	rcu_read_lock();
+	task=pid_task(find_vpid(nr), PIDTYPE_PID);
+	rcu_read_unlock();
+	return task;
 }
 
+
+void registration_handler(char *buf);
+void yield_handler(char *buf);
+void de_registration_handler(char *buf);
 static ssize_t mp2_write (struct file *file, const char __user *buffer, size_t count, loff_t *data);
 static ssize_t mp2_read (struct file *file, char __user *buffer, size_t count, loff_t *data);
 int __init mp2_init(void);
