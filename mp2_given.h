62346a48f61ac413859cfeceda3525353fb98cb9
LegendCHN <ningkaiwu@gmail.com>
2019-03-26 04:49:33 -0500

    adding reg handler

--- mp2_given.h
+++ mp2_given.h
@@ -29,7 +29,7 @@ struct task_struct* find_task_by_pid(unsigned int nr)
 }
 
 int admission_control(unsigned long period, unsigned long computation);
-struct linkedlist* find_linkedlist_by_pid(unsigned int pid);
+struct linkedlist* find_linkedlist_by_pid(unsigned long data);
 struct linkedlist *get_best_ready_task(void);
 void free_linkedlist(void);
 int dispatching_t_fn(void *data);
